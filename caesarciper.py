971fdc378bd0556b90c1d45ee1f91d755a5466ca
N.B K SARMA <135017029+N-B-K-SARMA@users.noreply.github.com>
2026-09-10 23:14:36 +0530

    Fix variable name for encrypt message output

--- caesarciper.py
+++ caesarciper.py
@@ -25,7 +25,7 @@ def decrypt(text, key):
 text = input("Enter message: ")
 key = int(input("Enter key: "))
 
-encrypted = encrypt(text, key)
-print("Encrypted:", encrypted)
+encrypt = encrypt(text, key)
+print("Encrypted:", encrypt)
 
-print("Decrypted:", decrypt(encrypted, key))
+print("Decrypted:", decrypt(encrypt, key))
